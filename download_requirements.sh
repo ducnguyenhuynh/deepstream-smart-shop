e3f4a0c3279466bd2ab7687032953a5ebde02828
ducnguyenhuynh <ducnghh@gmail.com>
2021-11-01 16:18:44 +0000

    add requirements

---
+++ download_requirements.sh
@@ -0,0 +1,4 @@
+wget https://github.com/ducnguyenhuynh/deepstream-smart-shop/releases/download/v1.0/engines.zip
+unzip engines.zip
+rm engines.zip
+wget https://github.com/ducnguyenhuynh/deepstream-smart-shop/releases/download/v1.0/store.h264
